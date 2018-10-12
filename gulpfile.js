b8c006a51c2d4f24b97302f529ecf414ffbefe6b
DEV-SAM\Sam Quan <sam.quan@wontok.com>
2018-10-12 14:18:25 +1100

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
 var changed = require('gulp-changed');
-
+var browsersync = require('browser-sync').create();
 
 gulp.task('hello', function() {
     
@@ -23,8 +23,27 @@ gulp.task('imagemin', function() {
 
 gulp.task('sass', function() {
 
-    return gulp.src('Src/Styles/**/*')
+    return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('Build/Styles'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browsersync.reload({
+        stream: true
+    }))
 });
 
+gulp.task ('watch', ['browserSync', 'sass'],  function(){
+    
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+
+    gulp.watch('app/*.html', browsersync.reload);
+    gulp.watch('app/js/**/*.js', browsersync.reload);
+
+})
+
+gulp.task('browserSync', function() {
+    browsersync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+})
